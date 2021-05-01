361e1997df18ec0c319eeef2fce16457ee123950
azmec <pokekights6702@gmail.com>
2021-05-01 17:37:20 -0500

    Created "stationary_pool" pool to account for walls.

--- love-project/src/systems/moveSystem.lua
+++ love-project/src/systems/moveSystem.lua
@@ -6,38 +6,65 @@ local Signal = require "libs.hump.signal"
 local Concord = require "libs.concord"
 Concord.utils.loadNamespace("src/components")
 
-local collisionWorld = bump.newWorld(32)
 
 local moveSystem = Concord.system({
-	pool = {"position", "velocity", "collision"}
+	moving_pool = {"position", "velocity", "collision"},
+	stationary_pool = {"position", "collision"}
 })
 
-function moveSystem:init(world)
-	local pool = self.pool
-	function pool:onEntityAdded(entity)
-		if collisionWorld:hasItem(entity) then
+-------------
+-- BUMP WORLD
+-------------
+
+local collisionWorld = bump.newWorld(32)
+
+local function add_to_collisionWorld(entity)
+	if collisionWorld:hasItem(entity) then
 			return
-		end
-
-		collisionWorld:add(
-			entity,
-			entity.position.x,
-			entity.position.y,
-			entity.collision.width,
-			entity.collision.height)
-
 	end
-	function pool:onEntityRemoved(entity)
-		if not collisionWorld:hasItem(entity) then
-			return
-		end 
-		
-		collisionWorld:remove(entity)
+
+	collisionWorld:add(
+		entity,
+		entity.position.x,
+		entity.position.y,
+		entity.collision.width,
+		entity.collision.height)
+end
+
+local function remove_from_collisionWorld(entity)
+	if not collisionWorld:hasItem(entity) then
+		return
+	end 
+	collisionWorld:remove(entity)
+end
+
+-----------------
+-- END BUMP WORLD
+-----------------
+
+function moveSystem:init(world)
+	-- As a note, this isn't particularly efficient. We're checking
+	-- twice per entity added to account for something as simple as
+	-- immovable walls, but it's not too detrimental.
+	local moving_pool = self.moving_pool
+	function moving_pool:onEntityAdded(entity)
+		add_to_collisionWorld(entity)
+	end
+	function moving_pool:onEntityRemoved(entity)
+		remove_from_collisionWorld(entity)
+	end
+
+	local stationary_pool = self.stationary_pool
+	function stationary_pool:onEntityAdded(entity)
+		add_to_collisionWorld(entity)
+	end
+	function stationary_pool:onEntityRemoved(entity)
+		remove_from_collisionWorld(entity)
 	end
 end
 
 function moveSystem:update(delta)
-	for _, entity in ipairs(self.pool) do
+	for _, entity in ipairs(self.moving_pool) do
 		-- Clamping velocity before doing any math
 		entity.velocity.x = simple.clamp(entity.velocity.x, -entity.physics.max_speed.x, entity.physics.max_speed.x)
 		entity.velocity.y = simple.clamp(entity.velocity.y, -entity.physics.max_speed.y, entity.physics.max_speed.y)

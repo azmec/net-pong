ee01ef9f0b45f2f7fb35018ceb6ed8ba5c5c2fdd
azmec <pokekights6702@gmail.com>
2021-05-05 20:51:12 -0500

    Initialized onlineGame.

---
+++ love-project/src/gamestates/onlineGame.lua
@@ -0,0 +1,27 @@
+local onlineGame = {}
+
+function onlineGame:init()
+
+end
+
+function onlineGame:update(delta)
+
+end
+
+function onlineGame:draw()
+
+end
+
+function onlineGame:keyreleased(key, scancode, isrepeat)
+
+end 
+
+function onlineGame:mousepressed(x, y, button)
+
+end
+
+function onlineGame:mousereleased(x, y, button)
+
+end
+
+return onlineGame

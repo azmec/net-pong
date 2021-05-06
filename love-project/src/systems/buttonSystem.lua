e5b3df2a37919f09504078b6b2a7a3c28e93ed37
azmec <pokekights6702@gmail.com>
2021-05-05 19:54:04 -0500

    Documented buttonSystem.

--- love-project/src/systems/buttonSystem.lua
+++ love-project/src/systems/buttonSystem.lua
@@ -1,5 +1,6 @@
--- Looking at this, there's probably something to be said
--- for a component that does nothing by itself. Oh well.
+-- Cycles through all entities with a button-like
+-- components and checks for their being pressed,
+-- hovered, and such.
 
 local Concord = require "libs.concord"
 local palette = require "src.palette"
@@ -9,18 +10,23 @@ local buttonSystem = Concord.system({
 	pool = {"position", "mouse_collision", "sprite", "button"}
 })
 
+-- Cycling through all relevant entities to
+-- determine if they're being selected or pressed.
 function buttonSystem:update(delta)
 	for _, entity in ipairs(self.pool) do
 		local sprite = entity.sprite
 		local button = entity.button
 		local mouse_collision = entity.mouse_collision
 
-		--button.is_selected = mouse_collision.is_colliding
 		if mouse_collision.is_colliding or button.is_selected then
+			-- Altering button color
 			sprite.color = button.highlighted_color
 			button.pressed = mouse_collision.left_clicked
 
+			-- If we either release left-click or "return"
+			-- while the button is selected
 			if mouse_collision.left_released or confirmed then
+				-- Emit for other entity to handle.
 				button.signal:emit("pressed")
 			end
 			if button.pressed then
@@ -31,8 +37,9 @@ function buttonSystem:update(delta)
 			sprite.color = button.default_color
 		end
 
+		-- If the user is hovering and selecting buttons, give
+		-- preference to the mouse.
 		if mouse_collision.is_colliding and button.is_selected then
-			-- give preference to the mouse
 			button.is_selected = false
 		end
 	end
@@ -40,6 +47,7 @@ function buttonSystem:update(delta)
 	confirmed = false
 end  
 
+-- Drawing the font for each button.
 function buttonSystem:draw()
 	local font = love.graphics.newFont("assets/renogare-regular.otf", 16)
 	love.graphics.setFont(font)
@@ -54,6 +62,8 @@ function buttonSystem:draw()
 	end
 end
 
+-- While we're handling keyinput here, some thought should
+-- be given to offloading this behavior.
 function buttonSystem:keyreleased(key, scancode, isrepeat)
 	if key == "return" or key == "space" then
 		confirmed = true
